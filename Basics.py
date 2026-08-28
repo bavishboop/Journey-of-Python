c7abfae378b87d43f94ef7b6ad622bf721d05932
Bavish <bavish2984@gmail.com>
2026-08-28 12:24:26 +0530

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,35 @@
+name = input("Enter your name")
+print("Hello ,",name,"! Welcome !") # print() function
+s = "anjelina"
+age = 25
+city = "new york"
+print(s, age, city) # use of variables
+x, y = input("Enter two numbers:  ").split()
+print(x, y)  # use of split function
+i = int(input("Enter a number: "))
+f = float(input("Evaluate 7/2: "))
+print(i,f)  # use of int() and float() functions
+x = 5
+name = "John"
+print(x)
+print(name)
+
+#variable assignment
+x = 5
+y = 3.14
+z = "hi"
+x = "now a string" #variable assignment
+print(x,y,z)
+a = b = c = 10
+print(a,b,c) #single value assignment to multiple variables
+x = 1 
+y = x
+y = y + 1
+print(x)
+print(y) #variable assignment
+a, b = 5, 10
+a, b = b, a
+print(a, b) #multiple value assignment
+word = "Python"
+length = len(word)
+print("Length of the word:", length) #use of len() function
